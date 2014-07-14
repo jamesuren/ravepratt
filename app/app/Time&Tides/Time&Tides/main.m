433283b9a5fe43b281d2164c1de67761894cc52a
Ngozi Okoro <nnokoro@gmail.com>
2014-07-14 13:15:48 +0100

    This is the first version of the app!

---
+++ app/app/Time&Tides/Time&Tides/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Time&Tides
+//
+//  Created by Ngozi Okoro on 7/14/14.
+//  Copyright (c) 2014 RavePratt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "raveprattAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([raveprattAppDelegate class]));
+    }
+}
---
+++ app/app/Time&Tides/Time&TidesTests/Time_TidesTests.m
@@ -0,0 +1,34 @@
+//
+//  Time_TidesTests.m
+//  Time&TidesTests
+//
+//  Created by Ngozi Okoro on 7/14/14.
+//  Copyright (c) 2014 RavePratt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Time_TidesTests : XCTestCase
+
+@end
+
+@implementation Time_TidesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
