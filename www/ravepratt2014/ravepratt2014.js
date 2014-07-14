e21e0f7755bf48e484133617ab811f4b778893bd
James Uren <juren@airsource.co.uk>
2014-07-14 14:53:31 +0100

    Started scoring logic

--- www/ravepratt2014/ravepratt2014.js
+++ www/ravepratt2014/ravepratt2014.js
@@ -32,7 +32,7 @@ if (Meteor.isClient) {
 
 	Template.journey.inStory = function () {
 		var story = Session.get("story");
-		if (story == -1){
+		if (story == -1) {
 			return false
 		}
 		return true;	
@@ -43,9 +43,7 @@ if (Meteor.isClient) {
 		var journey = Journies.findOne({_id: journeyId});
 		var quest = Session.get("quest");
 		var page = Session.get("story");
-		console.log(page);
 		var story = journey.quests[quest].story[page];
-		console.log("Loading story: " + story);
 		return story.background;
 	};
 	
@@ -54,37 +52,51 @@ if (Meteor.isClient) {
 		var journey = Journies.findOne({_id: journeyId});
 		var quest = Session.get("quest");
 		var page = Session.get("story");
-		console.log(page);
 		var story = journey.quests[quest].story[page];
-		console.log("Loading story: " + story);
 		return story.text;
 	};
 
-	Template.quest.question = function () {
+	Template.question.questionText = function () {
 		var journeyId = Session.get("journey");
 		var journey = Journies.findOne({_id: journeyId});
 		var quest = Session.get("quest");
 		var question = journey.quests[quest].question;
-		console.log("Loading question: " + question);
 		return question;
 	};
 
-	Template.quest.answers = function () {
+	Template.question.answers = function () {
 		var journeyId = Session.get("journey");
 		var journey = Journies.findOne({_id: journeyId});
 		var quest = Session.get("quest");
 		var answers = journey.quests[quest].answers;
-		console.log("Answers: " + answers);
     	return answers;
 	};
 
 	Template.answer.answer = function () {
 		return this;
-	}
+	};
+
+	Template.starsTotal.stars = function () {
+		return Session.get("stars");
+	};
 	
 	Template.answer.events( {
 		'click': function () {
 			console.log("Clicked answer " + this);
+			var journeyId = Session.get("journey");
+			var journey = Journies.findOne({_id: journeyId});
+			var quest = Session.get("quest");
+			var correctAnswer = journey.quests[quest].correctAnswer;
+			if (this == correctAnswer) 
+			if (page < storyLength) {
+				Session.set("story", page);
+			}
+			else {
+				Session.set("story", -1);
+			}
+			
+			// if this=the answer is correct then + 1 to quest, set story to zero and add reward to the stars
+			// if the answer is incorrect then - 1 from reward 
 		}
 	});
 	
@@ -120,7 +132,8 @@ if (Meteor.isClient) {
 				// Only start journey for character with quests
 		        Session.set("journey", this._id);
 				Session.set("quest", 0);
-		        Session.set("story", 0);			
+		        Session.set("story", 0);
+				Session.set("stars", 0);			
 			}
 			console.log("Selected character " + this.name);	
       	}
