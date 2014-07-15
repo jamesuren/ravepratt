d756ea648bf91fb503735c1612d983c6079f79dd
James Uren <juren@airsource.co.uk>
2014-07-15 09:31:06 +0100

    Tidied up and commented

--- www/ravepratt2014/ravepratt2014.js
+++ www/ravepratt2014/ravepratt2014.js
@@ -1,215 +1,10 @@
-// The common layout for all our routes
-/*
-Router.configure({});
-
-Router.map(function () {
-	this.route('overview', {
-	    path: '/',
-	    data: function () {
-	      // Do something...
-	    }
-  	});
-	this.route('', {
-	    path: '/',
-	    data: function () {
-	      // Get the current user
-	      var userId = this.params.userId
-		  console.log("User " + userId + " is online");
-	      return userId;
-	    }
-  	});
-});
-*/
-
+// Journies model
 Journies = new Meteor.Collection("journies");
-// Users = new Meteor.Collection("users");
 
+/*
+ * CLIENT SIDE
+ */
 if (Meteor.isClient) {
-
-	Template.journies.hasSelectedJourney = function ()	{
-		return Session.get("journey");
-	};
-
-	Template.journey.inStory = function () {
-		var story = Session.get("story");
-		if (story == -1) {
-			return false
-		}
-		return true;	
-	};
-	
-	
-	Template.storyPreviousButton.buttonPreviousEnabled = function (){
-		var story = Session.get("story")
-		if (story == 0) {
-			return "disabled";
-		}
-		return "enabled";
-	};
-	
-	Template.storyNextButton.buttonNextEnabled = function (){
-		var story = Session.get("story");
-		var journeyId = Session.get("journey");
-		var journey = Journies.findOne({_id: journeyId});
-		var quest = Session.get("quest");
-		var storyLength = journey.quests[quest].story.length;
-		if (story == (storyLength - 1)) {
-			return "disabled";
-		}
-		return "enabled";
-	};
-		
-	Template.story.background = function () {
-		var journeyId = Session.get("journey");
-		var journey = Journies.findOne({_id: journeyId});
-		var quest = Session.get("quest");
-		var page = Session.get("story");
-		var story = journey.quests[quest].story[page];
-		return story.background;
-	};
-	
-	Template.story.text = function () {
-		var journeyId = Session.get("journey");
-		var journey = Journies.findOne({_id: journeyId});
-		var quest = Session.get("quest");
-		var page = Session.get("story");
-		var story = journey.quests[quest].story[page];
-		return story.text;
-	};
-	
-	Template.story.element = function () {
-		return this;
-	};
-	
-	Template.question.questionText = function () {
-		var journeyId = Session.get("journey");
-		var journey = Journies.findOne({_id: journeyId});
-		var quest = Session.get("quest");
-		var question = journey.quests[quest].question;
-		return question;
-	};
-
-	Template.question.answers = function () {
-		var journeyId = Session.get("journey");
-		var journey = Journies.findOne({_id: journeyId});
-		var quest = Session.get("quest");
-		var answers = journey.quests[quest].answers;
-    	return answers;
-	};
-
-	Template.answer.answer = function () {
-		return this;
-	};
-
-	Template.starsTotal.stars = function () {
-		return Session.get("stars");
-	};
-	
-	Template.popUp.popUpEnabled = function (){
-		var dialog = Session.get("dialog");
-		if (dialog){
-			return "enabled";
-		}
-		return "disabled";
-
-	}
-	
-	Template.popUp.popUpText = function () {
-		return Session.get("popUpText");
-	}
-	
-	Template.popUp.popUpButtonText = function () {
-		return Session.get("popUpButtonText");
-	}
-	
-	Template.stars.stars = function () {
-		return [0,1,2]; //TODO hack
-	}
-	
-	Template.answer.events( {
-		'click': function () {
-			console.log("Clicked answer " + this);
-			var journeyId = Session.get("journey");
-			var journey = Journies.findOne({_id: journeyId});
-			var quest = Session.get("quest");
-			var reward = Session.get("reward");
-			var stars = Session.get("stars");
-			var correctMessage = journey.quests[quest].correctMessage;
-			var correctAnswer = journey.quests[quest].correctAnswer;
-			Session.set("popUp", true);
-			if (this == correctAnswer) {
-				console.log("Clicked correct answer");
-				Session.set("quest", quest + 1);
-				Session.set("story", 0);
-				Session.set("stars", stars+reward);
-				Session.set("reward", 5);
-				Session.set("popUpText", correctMessage);
-				Session.set("popUpButtonText", "Continue");
-			}
-			else {
-				Session.set("reward", reward-1);				
-				if (reward > 1) {
-					Session.set("popUpText", "Try again!");
-					Session.set("popUpButtonText", "Try Again");
-				}
-				else {
-					Session.set("popUpText", "Totally wrong");
-					Session.set("popUpButtonText", "Continue");
-				}
-			}
-	
-			
-			// if this=the answer is correct then + 1 to quest, set story to zero and add reward to the stars
-			// if the answer is incorrect then - 1 from reward 
-		}
-	});
-	
-	Template.storyNextButton.events( {
-		'click': function () {
-			var journeyId = Session.get("journey");
-			var journey = Journies.findOne({_id: journeyId});
-			var quest = Session.get("quest");
-			var storyLength = journey.quests[quest].story.length;
-			var page = Session.get("story");
-			page = page + 1;
-			if (page < storyLength) {
-				Session.set("story", page);
-			}
-			else {
-				Session.set("story", -1);
-			}
-		}
-	});
-	
-	Template.storyPreviousButton.events( {
-		'click': function () {
-			var journeyId = Session.get("journey");
-			var journey = Journies.findOne({_id: journeyId});
-			var quest = Session.get("quest");
-			var storyLength = journey.quests[quest].story.length;
-			var page = Session.get("story");
-			page = page - 1;
-			if (page < storyLength) {
-				Session.set("story", page);
-			}
-			else {
-				Session.set("story", -1);
-			}
-
-		}
-	});
-	
-	Template.hint.events( {
-		'click': function () {
-			var journeyId = Session.get("journey");
-			var journey = Journies.findOne({_id: journeyId});
-			var quest = Session.get("quest");
-			var hint = journey.quests[quest].hint;
-			console.log ("Hint: " + hint);
-			return quest.hint;	
-
-		}
-	});
 	
 	/*
 	 * Character selection
@@ -217,12 +12,12 @@ if (Meteor.isClient) {
 	Template.characters.characters = function () {
     	return Journies.find();
 	};
-	
     Template.character.events( {
       	'click': function () {
+			// Only start journey for character with quests
 			if (this.quests)
 			{
-				// Only start journey for character with quests
+				// Start journey
 		        Session.set("journey", this._id);
 				Session.set("quest", 0);
 		        Session.set("story", 0);
@@ -233,15 +28,192 @@ if (Meteor.isClient) {
 			console.log("Selected character " + this.name);	
       	}
     });
+	
+	/*
+	 * State machine
+	 */
+	Template.journies.hasSelectedJourney = function ()	{
+		return Session.get("journey");
+	};
+	Template.journey.inStory = function () {
+		var page = Session.get("story");
+		if (page == -1) {
+			return false
+		}
+		return true;	
+	};
+	
+	/*
+	 * Events
+	 */
+	Template.answer.events( {
+		// Clicked an answer
+		'click': function () {
+			console.log("Clicked answer " + this);
+			var quest = Session.get("quest");
+			var reward = Session.get("reward");
+			var stars = Session.get("stars");
+			var correctMessage = getQuest().correctMessage;
+			var correctAnswer = getQuest().correctAnswer;
+			Session.set("popUp", true);
+			if (this == correctAnswer) {
+				// Correct answer
+				console.log("Clicked correct answer");
+				Session.set("quest", quest + 1);
+				Session.set("story", 0);
+				Session.set("stars", stars+reward);
+				Session.set("reward", 5);
+				Session.set("popUpText", correctMessage);
+				Session.set("popUpButtonText", "Continue");
+			}
+			else {
+				// Wrong answer
+				Session.set("reward", reward-1);				
+				if (reward > 1) {
+					// Show try again pop-up
+					Session.set("popUpText", "Try again!");
+					Session.set("popUpButtonText", "Try Again");
+				}
+				else {
+					// No more try-agains. Show continue pop-up
+					Session.set("popUpText", "Totally wrong");
+					Session.set("popUpButtonText", "Continue");
+				}
+			}
+		}
+	});
+	Template.storyNextButton.events( {
+		// Clicked the next button
+		'click': function () {
+			var storyLength = getQuest().story.length;
+			var page = Session.get("story");
+			page = page + 1;
+			if (page < storyLength) {
+				Session.set("story", page);
+			}
+			else {
+				Session.set("story", -1);
+			}
+		}
+	});	
+	Template.storyPreviousButton.events( {
+		// Clicked the previous button
+		'click': function () {
+			var storyLength = getQuest().story.length;
+			var page = Session.get("story");
+			page = page - 1;
+			if (page < storyLength) {
+				Session.set("story", page);
+			}
+			else {
+				Session.set("story", -1);
+			}
 
+		}
+	});	
+	Template.hint.events( {
+		// Clicked the hint button
+		'click': function () {
+			var hint = getQuest().hint;
+			console.log ("Hint: " + hint);
+			return hint;	
+		}
+	});
+	
+	/*
+	 * Button states
+	 */
+	Template.storyPreviousButton.buttonPreviousEnabled = function (){
+		var page = Session.get("story")
+		if (page == 0) {
+			return "disabled";
+		}
+		return "enabled";
+	};	
+	Template.storyNextButton.buttonNextEnabled = function () {
+		var page = Session.get("story");
+		var storyLength = getQuest().story.length;
+		if (page == (storyLength - 1)) {
+			return "disabled";
+		}
+		return "enabled";
+	};
+	
+	/*
+	 * Data hooks
+	 */
+	Template.story.background = function () {
+		return getStory().background;
+	};	
+	Template.story.text = function () {
+		return getStory().text;
+	};
+	Template.story.element = function () {
+		return this;
+	};
+	Template.question.questionText = function () {
+		return getQuest().question;
+	};
+	Template.question.answers = function () {
+    	return getQuest().answers;
+	};
+	Template.answer.answer = function () {
+		return this;
+	};
+	Template.starsTotal.stars = function () {
+		return Session.get("stars");
+	};	
+	
+	/*
+	 * Pop Up Dialog Box
+	 */
+	Template.popUp.popUpEnabled = function (){
+		var dialog = Session.get("dialog");
+		if (dialog){
+			return "enabled";
+		}
+		return "disabled";
+	};
+	Template.popUp.popUpText = function () {
+		return Session.get("popUpText");
+	};	
+	Template.popUp.popUpButtonText = function () {
+		return Session.get("popUpButtonText");
+	};	
+	Template.stars.stars = function () {
+		return [0,1,2]; //TODO hack for now
+	};
+	
+	/*
+	 * Helpers
+	 */
+	function getJourney()
+	{
+		var journeyId = Session.get("journey");
+		return Journies.findOne({_id: journeyId});
+	}
+	function getQuest()
+	{
+		var quest = Session.get("quest");
+		return getJourney().quests[quest];
+	}
+	function getStory()
+	{
+		var page = Session.get("story");
+		return getQuest().story[page];
+	}
 }
 
+/*
+ * SERVER SIDE
+ */
 if (Meteor.isServer) {
   	Meteor.startup(function () {
+		// Clear and add the journey data
 		Journies.remove( { } );
     	var data = [{
-			name: "Henry Davenport",
-     	   	image: "henry.png",
+			name: "Tom Binnacle",
+     	   	image: "tom.png",
 			description: "A 12 year old orphan fascinated by technology.",
      		quests: [{
      			question: "What is your name?",
@@ -294,9 +266,6 @@ if (Meteor.isServer) {
 			name: "Chip the Ship Chef",
 	 	   	image: "chef.png"
 		}, {
-			name: "Tom Binnacle",
-	 	   	image: "tom.png"
-     	}, {
 			name: "Textile Designer",
      	   	image: "textile.png"
      	}];
